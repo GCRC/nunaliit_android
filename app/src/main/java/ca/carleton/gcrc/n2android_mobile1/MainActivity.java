ddd4996fb633361783c0d5d6239077ffa2abebd9
Jean-Pierre Fiset <jp@fiset.ca>
2016-03-11 22:39:30 -0500

    Add action bar menu

--- app/src/main/java/ca/carleton/gcrc/n2android_mobile1/MainActivity.java
+++ app/src/main/java/ca/carleton/gcrc/n2android_mobile1/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
@@ -48,6 +51,25 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_manage_connections:
+                startConnectionListActivity(null);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void startCordova(View view){
