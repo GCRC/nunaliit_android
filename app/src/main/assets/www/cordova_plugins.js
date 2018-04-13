256c03ac19320a68244a4c20bc216d708f37eae7
ian serfaty <ian.serfaty@mindsea.com>
2018-04-13 17:38:44 -0300

    Add the media plugin and enable audio recordings.

--- app/src/main/assets/www/cordova_plugins.js
+++ app/src/main/assets/www/cordova_plugins.js
@@ -249,5 +249,22 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
             "file": "plugins/org.apache.cordova.mediacapture/www/helpers.js",
             "id": "org.apache.cordova.mediacapture.helpers"
         },
+
+
+
+        {
+            "file": "plugins/org.apache.cordova.media/www/Media.js",
+            "id": "org.apache.cordova.media.Media",
+            "clobbers": [
+                "window.Media"
+            ]
+        },
+        {
+            "file": "plugins/org.apache.cordova.media/www/MediaError.js",
+            "id": "org.apache.cordova.media.MediaError",
+            "clobbers": [
+                "window.MediaError"
+            ]
+        },
     ];
 });
